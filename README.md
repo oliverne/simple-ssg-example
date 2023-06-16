2973c1becdcbf8f508efead853e97e74eeeb1b0c
oliverne <oliverne@gmail.com>
2023-06-16 21:15:49 +0900

    프로젝트 세팅

---
+++ README.md
@@ -0,0 +1,36 @@
+# Static Site Generation 샘플
+
+Static Website를 만들어보자.
+
+하나의 템플릿에서 언어별 데이터를 읽어 여러개의 사이트를 생성한다.
+
+1. 템플릿 파일을 읽는다.
+1. JSON 파일을 읽는다.
+1. JSON 데이터를 템플릿 파일과 결합하여 HTML 파일로 저장한다.
+1. 웹서버에 배포하면 끝
+
+## 준비물
+
+- Node.js v18 이상
+
+## 개발
+
+1. 소스 체크아웃
+1. `npm install`
+1. `npm run build`
+1. `dist` 폴더를 웹서버에 배포
+
+`src/main.js`를 읽어보자.
+
+---
+
+## JS 모듈
+
+이 소스는 ES Module을 사용한다. `package.json`에 `"type": "module"`로 설정되어 있음.
+
+- [ESM, CommonJS 차이](https://velog.io/@kakasoo/ESM%EA%B3%BC-CommonJS%EC%9D%98-%EC%B0%A8%EC%9D%B4)
+
+### Template
+
+[JS 템플릿의 간단한 원리](https://medium.com/wdstack/understanding-javascript-micro-templating-f37a37b3b40e)를 봐봤자 이해가 잘안되면 그냥 수많은 라이브러리가 있으니 [Mustache](https://github.com/janl/mustache.js/)를 사용해보자.
+`npm install mustache`로 설치하자.
---
+++ src/main.js
@@ -0,0 +1,57 @@
+// @ts-check
+import fs from "node:fs";
+import path from "node:path";
+import Mustache from "mustache";
+
+/** Node.js 실행될 때 경로 */
+const dirname = path.resolve();
+
+/** 파일 시스템 절대 경로 반환 **/
+const getPath = (...args) => path.join(dirname, ...args);
+
+const settings = [
+  { lang: "en-us", data: "data.en.json", result: "/en-us/index.html" },
+  { lang: "ko-kr", data: "data.ko.json", result: "/ko-kr/index.html" },
+  // ...다른 언어 계속 추가 예정이라고 가정
+];
+
+const DIST = "dist";
+const SRC = "src";
+const PUBLIC = "public";
+
+// dist 폴더가 있다면 삭제
+if (fs.existsSync(getPath(DIST))) {
+  fs.rmSync(getPath(DIST), { recursive: true, force: true });
+}
+
+try {
+  for (const item of settings) {
+    // dist 언어별로 폴더를 만들어놔야, 나중에 파일을 경로별로 저장할 때 에러가 안남
+    fs.mkdirSync(getPath(DIST, item.lang), { recursive: true });
+  }
+} catch {} // 에러나면 무시
+
+// Mustache 템플릿 파일을 읽어 스트링으로 변환
+const templateFile = "index.template.html";
+const template = fs.readFileSync(getPath(PUBLIC, templateFile)).toString();
+
+// 언어별로 반복
+for (const item of settings) {
+  // JSON 파일을 읽어 binary => string으로 변환
+  const rawData = fs.readFileSync(getPath(SRC, item.data)).toString();
+
+  // string을 JS object 로 변환
+  const context = JSON.parse(rawData);
+
+  // SEE: https://github.com/janl/mustache.js/
+  // Mustache 템플릿 함수를 이용하여 템플릿 스트링에서 context를 치환하여 html 스트링 생성
+  const rendered = Mustache.render(template, context);
+
+  // 결과물을 파일로 저장
+  fs.writeFileSync(getPath(DIST, item.result), rendered);
+  fs.cpSync(getPath(PUBLIC, item.lang), getPath(DIST, item.lang), {
+    recursive: true,
+  });
+}
+
+console.log("Completed! Check out `dist` folder");
